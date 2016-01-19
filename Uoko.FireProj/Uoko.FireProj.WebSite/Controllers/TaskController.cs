3adca6c4cc1685feae62a645c13a9542404f9f50
zhiping <zhiping@uoko.com>
2016-01-19 15:41:46 +0800

    整体 布局 更改

---
+++ Uoko.FireProj/Uoko.FireProj.WebSite/Controllers/TaskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Uoko.FireProj.WebSite.Controllers
+{
+    public class TaskController : BaseController
+    {
+        // GET: Task
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
