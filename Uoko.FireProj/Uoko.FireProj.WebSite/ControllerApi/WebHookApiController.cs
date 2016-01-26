59034ada0a432a8b3b31cc2028678255e6bc36f5
zhiping <zhiping@uoko.com>
2016-01-26 14:58:19 +0800

    添加WebHook

---
+++ Uoko.FireProj/Uoko.FireProj.WebSite/ControllerApi/WebHookApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Uoko.FireProj.WebSite.ControllerApi
+{
+    [RoutePrefix("api/WebHookApi")]
+    public class WebHookApiController : BaseApiController
+    {
+        [HttpPost]
+        public IHttpActionResult BuildCallback(HttpRequestMessage req)
+        {
+            var content = req.Content.ReadAsStringAsync().Result;
+            var request = Request.GetRequestContext();
+            return Ok();
+        }
+    }
+}
