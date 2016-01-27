be94e7903cff1145a94c64627d6d8f265bde7944
zhiping <zhiping@uoko.com>
2016-01-27 11:35:48 +0800

    添加Build Hook 回调对象

--- Uoko.FireProj/Uoko.FireProj.WebSite/ControllerApi/WebHookApiController.cs
+++ Uoko.FireProj/Uoko.FireProj.WebSite/ControllerApi/WebHookApiController.cs
@@ -4,17 +4,46 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Uoko.FireProj.Abstracts;
+using Uoko.FireProj.DataAccess.Gitlab;
 
 namespace Uoko.FireProj.WebSite.ControllerApi
 {
     [RoutePrefix("api/WebHookApi")]
     public class WebHookApiController : BaseApiController
     {
-        [HttpPost]
-        public IHttpActionResult BuildCallback(HttpRequestMessage req)
+        private ITaskSvc _taskSvc { get; set; }
+        private ITaskLogsSvc _taskLogsSvc { get; set; }
+        public WebHookApiController(ITaskSvc taskSvc, ITaskLogsSvc taskLogsSvc)
         {
-            var content = req.Content.ReadAsStringAsync().Result;
-            var request = Request.GetRequestContext();
+            _taskSvc = taskSvc;
+            _taskLogsSvc = taskLogsSvc;
+        }
+
+        /// <summary>
+        /// 触发编译回调
+        /// </summary>
+        /// <param name="bhRequest"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult BuildCallback(BuildHookRequest bhRequest)
+        {
+            if (!"object_kind".Equals(bhRequest.object_kind, StringComparison.OrdinalIgnoreCase)|| bhRequest.trigger_request_id==0)
+            {
+                return Ok();
+            }
+
+            //部署失败
+            if ("failed".Equals(bhRequest.build_status, StringComparison.OrdinalIgnoreCase))
+            {
+                
+            }
+            //部署成功
+            if ("success".Equals(bhRequest.build_status, StringComparison.OrdinalIgnoreCase))
+            {
+
+            }
+
             return Ok();
         }
     }
