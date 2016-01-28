772b1914704a07bbf4ba481b63f05e2f8aaa5af0
qingpan <qingpan@uoko.com>
2016-01-28 10:57:54 +0800

    调整项目管理CURD独立页面

---
+++ Uoko.FireProj/Uoko.FireProj.WebSite/app/controllers/projectCreat.js
@@ -0,0 +1,30 @@
+﻿
+//创建一个ProjectController
+fireproj.controller("ProjectController", function ($scope, $http, ProjectService) {
+  
+    //声明表单提交事件
+    $scope.SubmitFrom = function (model) {
+        var gitlabInfo = JSON.parse(model.Project);
+        model.ProjectRepo = gitlabInfo.http_url_to_repo;
+        model.ProjectId = gitlabInfo.id;
+        model.ProjectGitlabName = gitlabInfo.name;
+        ProjectService.post(model).success(function (data) {
+            formSubmitSuccessClick();
+        }).error(function (data) {
+            formSubmitFailClick(data);
+        });
+    };
+
+    //获取gitlab所有项目信息
+    ProjectService.getGitlbProject().success(function (data) {
+        $scope.ProjectList = data;
+    }).error(function (data) {
+        formSubmitFailClick(data);
+    });
+
+    $scope.CancelForm = function () {
+        location.href = "/Project/Index";
+    }
+});
+
+
