e67c4e4dde14160210ad117bc8e39d25f5ea94cc
zhiping <zhiping@uoko.com>
2016-01-28 12:02:02 +0800

    任务记录

--- Uoko.FireProj/Uoko.FireProj.WebSite/app/services/TaskService.js
+++ Uoko.FireProj/Uoko.FireProj.WebSite/app/services/TaskService.js
@@ -9,7 +9,16 @@
             //错误处理
         });;
     };
-
+    this.GetTaskLogsByPage = function (params, successCallBack) {
+        $http.get("/api/TaskLogsApi?Offset={0}&Limit={1}&taskId={2}".Format(params.offset, params.limit, params.taskId)).success(function (data) {
+            if (successCallBack != undefined) {
+                successCallBack(data);
+            }
+        }).error(function (data) {
+            //错误处理
+        });;
+    };
+    
     this.CreateTask = function (task, successCallBack) {
         $http.post("/api/TaskApi/Create", task).success(function (data) {
             if (successCallBack != undefined) {
