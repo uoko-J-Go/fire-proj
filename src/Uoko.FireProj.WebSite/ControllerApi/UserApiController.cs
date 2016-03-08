8c57f3152b73bdc1bd9f5db2daabdef2343348ba
zhiping <zhiping@uoko.com>
2016-03-08 12:08:21 +0800

    邮件发送基础 和相应配置修改

--- src/Uoko.FireProj.WebSite/ControllerApi/UserApiController.cs
+++ src/Uoko.FireProj.WebSite/ControllerApi/UserApiController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,9 +13,10 @@ namespace Uoko.FireProj.WebSite.ControllerApi
     [RoutePrefix("api/UserApi")]
     public class UserApiController : ApiController
     {
+        private WebApiProvider apiProvider = (new WebApiProvider(ConfigurationManager.AppSettings["system.api.url"]));
         public IHttpActionResult Get()
         {
-            var result = new WebApiProvider().Get<List<UserDto>>("http://api.systemset.uoko.pre/api/UserOld/10000/9/ByCompanyDepId");
+            var result = apiProvider.Get<List<UserDto>>("UserOld/10000/9/ByCompanyDepId");
             return Ok(result);
         }
     }
