ae9acbf274bb1ccc4a6eb61b9c60a25a16505451
zhiping <zhiping@uoko.com>
2016-03-18 17:23:46 +0800

    添加日志记录

---
+++ src/Uoko.FireProj.WebSite/Filter/LogExceptionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+
+namespace Uoko.FireProj.WebSite.Filter
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public class LogExceptionAttribute : HandleErrorAttribute
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled)
+            {
+                string controllerName = (string)filterContext.RouteData.Values["controller"];
+                string actionName = (string)filterContext.RouteData.Values["action"];
+                logger.Error(filterContext.Exception,string.Format("Controller:{0}| Action:{1}| Message:{2}", controllerName, actionName, filterContext.Exception.Message));
+                base.OnException(filterContext);
+            }
+        }
+    }
+}
